daf5616db0d7d7128405dfaff8855ea33ce4818a
Manish Kumar <83913865+Manish-prog-bit@users.noreply.github.com>
2022-05-22 16:15:35 +0530

    Add files via upload

---
+++ Assignment1(19105109)/Output of Assignment1.py
@@ -0,0 +1,43 @@
+QUESTION-1
+Please Enter the First Number:7
+Please Enter the second number:8
+Please Enter the third number:9
+The average of three numbers is 8.0
+QUESTION-2
+Enter your gross income in dollars 
+60000
+Enter number of dependents
+7
+Your total tax:
+ 5800.0
+QUESTION-3
+Enter seconds:600
+10 minutes and 0 seconds
+QUESTION-4
+75
+<class 'str'>
+QUESTION-5
+0  ---  0.0      1.0
+15  ---  0.2588          0.9659
+30  ---  0.5     0.866
+45  ---  0.7071          0.7071
+60  ---  0.866   0.5
+75  ---  0.9659          0.2588
+90  ---  1.0     0.0
+105  ---  0.9659         -0.2588
+120  ---  0.866          -0.5
+135  ---  0.7071         -0.7071
+150  ---  0.5    -0.866
+165  ---  0.2588         -0.9659
+180  ---  0.0    -1.0
+195  ---  -0.2588        -0.9659
+210  ---  -0.5   -0.866
+225  ---  -0.7071        -0.7071
+240  ---  -0.866         -0.5
+255  ---  -0.9659        -0.2588
+270  ---  -1.0   -0.0
+285  ---  -0.9659        0.2588
+300  ---  -0.866         0.5
+315  ---  -0.7071        0.7071
+330  ---  -0.5   0.866
+345  ---  -0.2588        0.9659
